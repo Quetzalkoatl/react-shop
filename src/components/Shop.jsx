6131451fd27f1ae5acb347de3ceb84d4f8b55999
Gotrunn <itsgotrunn@gmail.com>
2022-06-15 12:20:36 +0300

    changes

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -1,5 +1,33 @@
+import {useEffect, useState} from 'react';
+import {API_KEY, API_URL} from '../config';
+import Preloader from './Preloader';
+import GoodsList from './GoodsList';
+import Cart from './Cart';
+
 const Shop = () => {
-	return <main className='container content'>Shop</main>;
+	const [goods, setGoods] = useState([]);
+	const [loading, setLoading] = useState(true);
+	const [order, setOrder] = useState([]);
+
+	useEffect(function getGoods() {
+		fetch(API_URL, {
+			headers: {
+				Authorization: API_KEY,
+			},
+		})
+			.then(resp => resp.json())
+			.then(data => {
+				data.featured && setGoods(data.featured);
+				setLoading(false);
+			});
+	}, []);
+
+	return (
+		<main className='container content'>
+			<Cart quantity={order.length || '0'} />
+			{loading ? <Preloader /> : <GoodsList goods={goods} />}
+		</main>
+	);
 };
 
 export default Shop;
